6b9ae3f23d0bf610c3e8c6ec92b552d127347d0e
koala1707 <m3tpfbzr@gmail.com>
2024-06-29 10:05:08 +0930

    add routes

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,29 +1,30 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   // @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
-      
+
   }
 
   register() {
     this.accountService.register(this.model).subscribe({
       next: response => {
-        console.log(response); 
+        console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => { this.toastr.error(error.error), console.log(error) }
     })
     console.log(this.model);
   }
